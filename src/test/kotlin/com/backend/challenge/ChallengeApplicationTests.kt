203b7478004df6a3e54b0f87be628b1b10391994
Wellington Vieira <wellington.vieira@fatec.sp.gov.br>
2024-06-13 22:27:56 -0300

    [feat] - first commit

---
+++ src/test/kotlin/com/backend/challenge/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
